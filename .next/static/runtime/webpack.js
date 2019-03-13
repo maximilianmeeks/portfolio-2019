072d24b8c8c6b1e7132831e95ad7fb8854df6ee2
Maximilian <maximilian.meeks@gmail.com>
2019-03-13 15:52:33 +0100

    removed unneccesary vh-100

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0d5d656fd2b4d9f7cf21";
+/******/ 	var hotCurrentHash = "6dfff33741bf199fbc40";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
