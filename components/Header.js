33dc4fd0655b485cee00e2e28a1f67aec478688f
Dorschnase <maximilian.meeks@gmail.com>
2019-04-05 18:24:33 +0200

    header fisch and breakpoints added

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,25 @@
 import React from "react";
 import Link from "next/link";
-import { Container } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 
 const Header = () => (
     
     <Container className="bg-light pt-3 position-relative">
-        <img src="/static/rauchbock.png" alt="" className="header-bock position-absolute"/>
-        <Link href="/">
-        <a className="text-decoration-none">
-            <h2 className="text-primary text-right text-lowercase custom-header">Maximilian Meeks</h2>
-            <div className="text-right">
-                <span className="text-secondary text-right">Junior Frontend Developer</span>
-            </div>
-        </a>
-        </Link>
-    
+        <Row>
+            <Col xs="2">
+                <img src="/static/headerfisch.png" alt="" className="header-fisch"/>
+            </Col>
+            <Col xs="10">
+                <Link href="/">
+                    <a className="text-decoration-none">
+                        <h2 className="text-primary text-right text-lowercase custom-header">Maximilian Meeks</h2>
+                        <div className="text-right">
+                            <span className="text-secondary text-right">Junior Frontend Developer</span>
+                        </div>
+                    </a>
+                </Link>
+            </Col>
+        </Row>
     </Container>
 )
 
