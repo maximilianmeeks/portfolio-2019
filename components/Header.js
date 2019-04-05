ff2686dda349a1f332bad7f55d9e949fdaa5d735
Maximilian <maximilian.meeks@gmail.com>
2019-04-05 18:53:48 +0200

    minor header text edit

--- components/Header.js
+++ components/Header.js
@@ -14,7 +14,7 @@ const Header = () => (
                     <a className="text-decoration-none">
                         <h2 className="text-primary text-right text-lowercase custom-header">Maximilian Meeks</h2>
                         <div className="text-right">
-                            <span className="text-secondary text-right">Junior Frontend Developer</span>
+                            <span className="text-secondary text-right">Junior Frontend Web Developer</span>
                         </div>
                     </a>
                 </Link>
