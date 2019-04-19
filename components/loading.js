7bc3199f5ea65c29bf07a053c18b2e7b641fcd7b
Dorschnase <maximilian.meeks@gmail.com>
2019-04-19 18:41:44 +0200

    edited loading screen

--- components/loading.js
+++ components/loading.js
@@ -1,9 +1,11 @@
 import React, {Component} from "react";
 import { Container, Row, Col, Spinner } from "reactstrap";
+import Layout from "./Layout";
 
 export default class Loading extends Component {
     render() {
         return(
+            <Layout footer={false} header={false} menu={false}>
                 <Container className="vh-100">
                     <Row className="justify-content-center align-items-center">
                         <Col xs="12" className="d-flex justify-content-center align-items-center">
@@ -12,6 +14,7 @@ export default class Loading extends Component {
                         </Col>
                     </Row>
                 </Container>
+            </Layout>
         )
     }
 }
